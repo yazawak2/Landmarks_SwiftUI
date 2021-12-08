977a064e4046130ed4e7685b505c524dda346c89
JIRO <yazawa@sonicgarden.jp>
2021-12-08 16:19:22 +0900

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kojiro Yazawa on 2021/12/08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
