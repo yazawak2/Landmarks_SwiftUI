7526b3e3eff1653c96bd59ce0597f220d86b7846
JIRO <yazawa@sonicgarden.jp>
2021-12-16 10:17:39 +0900

    Building Lists and Navigation: Create a Landmark Model

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,33 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Kojiro Yazawa on 2021/12/16.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    var image: Image { // Computed property
+        Image(imageName)
+    }
+
+    private var coordinates: Coodinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
+
+    struct Coodinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
