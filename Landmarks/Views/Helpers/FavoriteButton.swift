5e6b5a801deb4674dd11ffd8823ca977ed052dae
JIRO <yazawa@sonicgarden.jp>
2021-12-24 15:46:18 +0900

    Handling User Input: Create a Favorite Button for Each Landmark

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Kojiro Yazawa on 2021/12/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favarite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
