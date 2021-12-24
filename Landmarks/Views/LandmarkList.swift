15df0cdc15af2062f25ff5eb35505aa625fbf43e
JIRO <yazawa@sonicgarden.jp>
2021-12-24 13:40:27 +0900

    Pass Data into Child Views

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Kojiro Yazawa on 2021/12/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
